bea8a9f20fc3915c9fb504a4053e2ace2fc674fe
Burak <burak@Burak-MacBook-Air.local>
2023-07-18 14:31:34 +0300

    The download of mail-list archives from the lore.kernel.org website based on subject titles has been completed. Currently, I am working on updating the mail lists.

--- mlsync/adapter.py
+++ mlsync/adapter.py
@@ -14,9 +14,9 @@ def find_mirror(url):
             link_url = link.get("href")
             return url + "/" + link_url
         else:
-            print("Hedef metin için bağlantı bulunamadı.")
+            print("Failed to reach the mirror link.")
     else:
-        print("Sayfa çekilemedi. Hata kodu:", response.status_code)
+        print("Page could not be fetched. Error code:", response.status_code)
 
 
 def find_git_clone_code(url, link_text):
@@ -51,6 +51,8 @@ def git_clone(url):
     else:
         error = result.stderr
         print("Hata mesajı:\n", error)
+
+
 def files_of_kernel():
     dizin = "/kernel"  # Dizin yolunu buraya girin
     dosya_adlari = os.listdir(dizin)
@@ -58,39 +60,63 @@ def files_of_kernel():
     for dosya_adi in dosya_adlari:
         yield dosya_adi
 
+
+def force_find_files_git_code(key_word):
+    url = "https://lore.kernel.org"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    links = soup.find_all("a")
+    href_next = ""
+    while True:
+        for link in links:
+            link_text = link.text.strip()
+            link_href = link.get("href")
+            if link_text == key_word:
+                return find_git_clone_code(find_mirror(url + "/" + link_href), key_word)
+            elif link_text == "next (older)":
+                href_next = link_href
+                url = "https://lore.kernel.org/" + href_next
+                response = requests.get(url)
+                soup = BeautifulSoup(response.content, "html.parser")
+
+                links = soup.find_all("a")
+
+
 def check_file_existence(file_name):
     return os.path.exists(file_name)
+
+
 def go_file(file_name):
 
-    result = subprocess.run(
-        ["cd " + "/" + file_name], shell=True,
-    )
+    result = subprocess.run(["cd " + "/" + file_name], shell=True,)
     if result.returncode == 0:
         output = result.stdout
         print(f"{file_name} adlı dosyaya girdi.")
     else:
         error = result.stderr
         print(f"{file_name} adlı dosyaya giremedi.Hata komutu:\n", error)
+
+
 def exit_file():
-    result = subprocess.run(
-        ["cd " + ".." +], shell=True,
-    )
+    result = subprocess.run(["cd " + ".."], shell=True,)
     if result.returncode == 0:
         output = result.stdout
         print("Dosyadan çıkış yapıldı.")
     else:
         error = result.stderr
         print("Dosyadan çıkılırken hata oldu. Hata komutu:\n", error)
+
+
 def pull_file():
-    result = subprocess.run(
-        ["git " + "pull" +], shell=True,
-    )
+    result = subprocess.run(["git " + "pull"], shell=True,)
     if result.returncode == 0:
         output = result.stdout
         print("Dosyadan çıkış yapıldı.")
     else:
         error = result.stderr
         print("Dosyadan çıkılırken hata oldu. Hata komutu:\n", error)
+
 
 def git_clone_kernel(url):  # url = "https://lore.kernel.org" #Bitmediiiiii !!!!
     response = requests.get(url)
@@ -121,6 +147,7 @@ def git_clone_kernel(url):  # url = "https://lore.kernel.org" #Bitmediiiiii !!!!
         if not "next (older)" in link_dict:
             a = files_of_kernel()
             for i in a:
+                pass
 
             for i in link_dict.keys():
                 if check_file_existence(i):
